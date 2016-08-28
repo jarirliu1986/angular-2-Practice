a614ebc00c1e534da70a20656645af52027f6257
jarirliu1986 <289374247@qq.com>
2016-08-28 17:48:02 -0400

    component practice

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {CourseComponent} from './courses.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>hello angular</h1>'
+    template: '<h1>hello angular</h1><courses></courses>',
+    directives: [CourseComponent]
 })
 export class AppComponent { }
